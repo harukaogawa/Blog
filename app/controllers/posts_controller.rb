7e37ab0a2ac1a6ffbf8b2302de0e0e87d14b891b
harukaogawa <60389657+harukaogawa@users.noreply.github.com>
2020-03-22 20:07:16 +0900

    投稿画面

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
   def index
+      # 投稿データを全て取得、またインスタンス変数なのでViewで参照可能
+      @posts = Post.all
+  end
+
+  def new
+      # Postモデルのオブジェクトを作成
+      # @boardはインスタンス変数で、Viewで参照可能
+      @post = Post.new
+  end
+
+  def create
+      # データはparamsという変数に渡されてくる
+      # create は、Postモデルのクラスメソッド
+      Post.create(post_params)
+      binding.pry
+  end
+
+  private
+
+  # paramsから欲しいデータのみ抽出
+  def post_params
+      params.require(:post).permit(:name, :title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get '', to: 'posts#index'
+  get 'posts', to: 'posts#index'
+
+  # 投稿ページを表示
+  get 'posts/new', to: 'posts#new'
+
+  # 投稿データ作成
+  post 'posts', to: 'posts#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
