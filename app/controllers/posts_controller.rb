bd91eacb5501732e1833313f7d13c57d71a16fe7
harukaogawa <60389657+harukaogawa@users.noreply.github.com>
2020-03-23 20:58:08 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,45 @@
 class PostsController < ApplicationController
   def index
-      # 投稿データを全て取得、またインスタンス変数なのでViewで参照可能
       @posts = Post.all
   end
 
   def new
-      # Postモデルのオブジェクトを作成
-      # @boardはインスタンス変数で、Viewで参照可能
       @post = Post.new
   end
 
+  # 修正
   def create
-      # データはparamsという変数に渡されてくる
-      # create は、Postモデルのクラスメソッド
-      Post.create(post_params)
-      binding.pry
+      post = Post.create(post_params)
+
+      # リダイレクト処理追加
+      redirect_to post
+  end
+
+  def show
+      @post = Post.find(params[:id])
+  end
+
+  def edit
+      @post = Post.find(params[:id])
+  end
+
+  def update
+      post = Post.find(params[:id])
+      post.update(post_params)
+      redirect_to post
+  end
+
+  # 削除機能
+  def destroy
+      post = Post.find(params[:id])
+      post.delete
+
+      # 投稿一覧へリダイレクト
+      redirect_to posts_path
   end
 
   private
 
-  # paramsから欲しいデータのみ抽出
   def post_params
       params.require(:post).permit(:name, :title, :content)
   end
