2f198488346e41f671832a6ad4fc4e412e159702
harukaogawa <60389657+harukaogawa@users.noreply.github.com>
2020-03-28 20:14:30 +0900

    コメント

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,17 +2,17 @@
 #
 # Table name: posts
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
 #  name       :string(255)
 #  title      :string(255)
-#  content    :text(65535)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class Post < ApplicationRecord
-  # presence -> 必須チェック
-  # length -> 文字数の制限
+  has_many :comments
+
   validates :name, presence: true, length: { maximum: 10 }
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 1000 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  # onlyを削除
   resources :posts
+  resources :comments, only: %i[create destroy]
 end
