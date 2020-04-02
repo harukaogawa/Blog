d8470366750c1a57c2cd883dbe0a898875bf2878
harukaogawa <60389657+harukaogawa@users.noreply.github.com>
2020-04-02 20:19:59 +0900

    タグ機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env == 'development'
+  (1..100).each do |i|
+      Post.create(name: "ユーザ#{i}", title: "タイトル#{i}", content: "本文#{i}")
+  end
+
+  Tag.create([
+      { name: '生活' },
+      { name: '仕事' },
+      { name: '学校' },
+      { name: 'お金' },
+      { name: '恋愛' }
+  ])
+end
